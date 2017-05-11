3de5e444648d8e16e27f46d7c1fa1d200bf909c4
beezebrown <m.brown298@gmail.com>
2017-05-11 13:25:22 -0400

    added the count-bases and make-double functions. 6 to go

--- p1.lisp
+++ p1.lisp
@@ -1,3 +1,13 @@
+(defun char-to-string (character)
+  (case character
+    (#\A (string "A"))
+    (#\T (string "T"))
+    (#\G (string "G"))
+    (#\C (string "C"))))
+
+(defun num-to-string (num)
+  (string num))
+
 (defun complement-base (base)
   (case base
     (#\A (string "T"))
@@ -16,3 +26,32 @@
 
 (defvar *strand* "AGTC")
 (complement-strand *strand*)
+
+(defun make-double (strand)
+(let ((double-strand (make-array 1 :adjustable t :fill-pointer 0)))
+  (loop for c across strand do
+    (vector-push-extend (concatenate 'string (char-to-string c) (complement-base c)) double-strand))
+    (print double-strand)))
+
+(make-double *strand*)
+
+(defun count-bases (strand)
+(setq numA 0)
+(setq numC 0)
+(setq numG 0)
+(setq numT 0)
+(loop for c across strand do
+  (case c
+    (#\A (setq numA (+ numA 1)))
+    (#\C (setq numC (+ numC 1)))
+    (#\G (setq numG (+ numG 1)))
+    (#\T (setq numT (+ numT 1)))))
+
+(print
+  (concatenate
+    'string "A" (write-to-string numA)
+    " C" (write-to-string numC)
+    " G" (write-to-string numG)
+    " T" (write-to-string numT))))
+
+(count-bases *strand*)
